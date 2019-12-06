96f92be7e464df44f0c3fcac620b42b0a67210b3
Alex Sander Foss <alexfoss@pec.local>
2019-12-06 16:46:41 -0300

    Django Tutorial Part 8: User authentication and permissions (without challenge)

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse  # Usado para gerar URLs, revertendo os padrões de URL
+from django.contrib.auth.models import User
+from datetime import date
+
 import uuid  # Necessário para instâncias de livros exclusivas
 
 
@@ -59,6 +62,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -77,6 +87,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String para representar o objeto Model."""
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -36,3 +36,8 @@ urlpatterns += [
 
 # Use static () para adicionar mapeamento de URL para servir arquivos estáticos durante o desenvolvimento (apenas)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
